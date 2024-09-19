7f145dc29fe59250d7e94e66edec1d5e50da33d1
Jan Ruhland <Jan.Ruhland@hhu.de>
2024-09-19 19:57:13 +0200

    Networks

---
+++ src/AutoEncSkinLesion.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 19:56:10 2024
+
+@author: jan
+"""
+
---
+++ src/TrainingSkinLesion.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 19:56:27 2024
+
+@author: jan
+"""
+
