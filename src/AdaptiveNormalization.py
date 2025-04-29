a97b0cfec08dc21c2cdbdd00dcc3e71669e65b5c
JanRland <153728756+JanRland@users.noreply.github.com>
2025-04-29 08:48:20 +0200

    Second version

--- src/AdaptiveNormalization.py
+++ src/AdaptiveNormalization.py
@@ -69,3 +69,62 @@ class AdaptiveNorm(nn.Module):
         else:
             x=torch.add(torch.mul(torch.div(torch.subtract(x,self.beta),self.gamma), torch.sqrt(self.relu(self.v)+self.eps)), self.m)
         return x  
+        
+        
+class RedAdaptiveNorm(nn.Module):
+    def __init__(self, dimensions, statisticWeights=False, offset=0):
+        super(AdaptiveNorm, self).__init__()
+        """
+        This is a simplified version of adaptive normalization, reducing 
+        the computational complexity of gradient descent from O(6kdn) to O(4kdn). 
+        Additionally, we replaced ReLU with GELU for variance estimation, 
+        helping to mitigate issues related to gradient discontinuities.
+
+
+        Parameters
+        ----------
+        dimensions : Tuple
+            Define the dimensions for which the normalization should be 
+            performed.
+        statisticWeights : Boolean, optional
+            If the initialized statistics is too high to update with gradient
+            descent, we introduce the scaling factor m_w, v_w. 
+            The default is False.
+        offset : Float
+            If an accurate estimate of the initial statistical moment is 
+            unavailable, you can tune it as a hyperparameter avoiding 
+            unstable training convergence. 
+
+        """
+
+        self.statWeights=statisticWeights
+
+
+        self.gamma=torch.nn.Parameter(torch.ones(*dimensions))
+        self.beta=torch.nn.Parameter(torch.zeros(*dimensions))
+        self.eps=0.0000001+0.17
+
+        self.gamma.requires_grad = True
+        self.beta.requires_grad = True
+
+        self.m=torch.zeros(*dimensions)+offset
+        self.v=torch.ones(*dimensions)
+
+        self.m_w=torch.nn.Parameter(torch.ones(*dimensions))
+        self.v_w=torch.nn.Parameter(torch.ones(*dimensions))
+
+        self.m_w.requires_grad = True
+        self.v_w.requires_grad = True
+
+        self.gelu=nn.GELU()
+
+    def initialize(self, m, v):
+        self.m=m
+        self.v=v
+
+    def forward(self, x):
+        if self.statWeights:
+            x=torch.add(torch.mul(torch.div(torch.subtract(x,self.m*self.m_w),torch.sqrt(self.gelu(self.v*self.v_w)+self.eps)), self.gamma), self.beta)
+        else:
+            x=torch.add(torch.mul(torch.div(torch.subtract(x,self.m),torch.sqrt(self.gelu(self.v)+self.eps)), self.gamma), self.beta)
+        return x
